816bf8ed8ff82145e7d80ab506a1627c744ede91
Local <localhost@localhost.com>
2023-03-05 11:55:06 -0700

    feat: renamed text format pipe

--- src/app/chat-container/chat-container.module.ts
+++ src/app/chat-container/chat-container.module.ts
@@ -6,21 +6,21 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { ChatContainerComponent } from './chat-container.component';
 import { ChatWriteFormComponent } from './chat-write-form/chat-write-form.component';
 import { ChatMessagesComponent } from './chat-messages/chat-messages.component';
 import { ChatSocketService } from './services/chat-socket.service';
 import { ChatService } from './services/chat.service';
 import { chatContainerRoutes } from './chat-container.routes';
-import { FlyingHeroesImpurePipe } from './text-format.pipe';
-import { MatCardModule } from '@angular/material/card';
+import { TextFormatPipe } from './text-format.pipe';
 
 @NgModule({
   declarations: [
     ChatContainerComponent,
     ChatMessagesComponent,
     ChatWriteFormComponent,
-    FlyingHeroesImpurePipe,
+    TextFormatPipe,
   ],
   imports: [
     RouterModule.forChild(chatContainerRoutes),
--- src/app/chat-container/text-format.pipe.ts
+++ src/app/chat-container/text-format.pipe.ts
@@ -3,11 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'textFormat',
 })
-export class FlyingHeroesImpurePipe implements PipeTransform {
+export class TextFormatPipe implements PipeTransform {
   transform(value: string): string {
     // Here can be improved by multiple regex
-    const val = value.replace(/\n/g, '<br/>').replace(/ /g, '&nbsp;');
-
-    return val;
+    return value.replace(/\n/g, '<br/>').replace(/ /g, '&nbsp;');
   }
 }
